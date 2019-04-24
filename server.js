5fdd1a5edb65dd2d49c585fea08a299920215fbc
Steve Garrison <stevegarrison@Steves-MacBook-Pro.local>
2019-04-24 14:57:10 -0600

    Pushing final

--- server.js
+++ server.js
@@ -18,6 +18,12 @@ app.use(express.static("public"));
 //require routes
 require("./routes/api-routes.js")(app);
 
+// If deployed, use the deployed database. Otherwise use the local mongoHeadlines database
+var MONGODB_URI = process.env.MONGODB_URI || "mongodb://localhost/mongoHeadlines";
+
+mongoose.connect(MONGODB_URI);
+
+
 // Listen on port 3000
 app.listen(3000, function () {
     console.log("App running on port 3000!");
